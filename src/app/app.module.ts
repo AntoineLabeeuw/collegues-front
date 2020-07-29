bf3131db8bdd8295dadeb4f1df6647c2b478e0c7
Antoine LABEEUW <antoine.labeeuw@gmail.com>
2020-07-29 15:37:09 +0200

    component tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbdAlertCloseable } from './ng-boostrap/ngbd'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdAlertCloseable
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, NgbdAlertCloseable]
 })
 export class AppModule { }

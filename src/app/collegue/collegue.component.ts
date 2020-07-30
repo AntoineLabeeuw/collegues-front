b71d2bbe3d44cc3e5dbaa44b8359d4a0efc73a6e
Antoine LABEEUW <antoine.labeeuw@gmail.com>
2020-07-30 11:22:30 +0200

    TP collegues #3 - Modifier - changed images urls

--- src/app/collegue/collegue.component.ts
+++ src/app/collegue/collegue.component.ts
@@ -8,14 +8,20 @@ import { Collegue } from '../models/Collegue';
 export class CollegueComponent implements OnInit {
   @Input()
   collegues: CollegueComponent;
+  edit = false;
   constructor() { }
   consoleModification(): void {
+    this.edit = !this.edit;
     console.log('Modification du collègue');
   }
   consoleCreation(): void {
     console.log(`Création d'un nouveau collègue`);
   }
+  consoleValider(email, photoUrl): void {
+    // in fine, sert a modifier les valeurs deja rentrées
+    this.edit = !this.edit;
+    console.log('Validation de la modif');
+  }
   ngOnInit(): void {
   }
-
 }
